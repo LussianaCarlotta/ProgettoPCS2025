12c3c8b279663fe31986c753323f3ebdeb4f0a8f
SofiaDiRosa <s297767@studenti.polito.it>
2025-05-16 09:24:06 +0200

    Modificato file CMakeLists.txt principale

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <iomanip>
 
+using namespace std;
+
 namespace PoliedriMesh
 {
 /*bool ImportMesh(PolygonalMesh& mesh)
@@ -23,5 +25,48 @@ namespace PoliedriMesh
 	return true;
 }*/
 //******************************************************************
+/*bool Cell0Ds(PoliedriMesh& mesh)
+{
+	ofstream file("./Cell0Ds.txt")
+	if(file.fail())
+	{
+		cerr << "Impossibile aprire il file in scrittura" << endl;
+		return false;
+	}
+	
+	
+	
+}*/
+
+PoliedriMesh creaTetraedroNormalizzato() {
+    PoliedriMesh mesh;
+
+    vector<Vector3d> vertici = {
+        {1.0, 1.0, 1.0},
+        {-1.0, -1.0, 1.0},
+        {-1.0, 1.0, -1.0},
+        {1.0, -1.0, -1.0}
+    };
+
+    mesh.NumCell0Ds = vertici.size();
+    mesh.Cell0DsId.resize(mesh.NumCell0Ds);
+    mesh.Cell0DsCoordinates.resize(3, mesh.NumCell0Ds);
+
+    for (unsigned int i = 0; i < vertici.size(); ++i) {
+        double x = vertici ;
+        double y = vertici ;
+        double z = vertici ;
+        double x_n = norm(x);
+		double y_n = norm(y);
+		double z_n = norm(z);
+        mesh.Cell0DsId[i] = idOffset + i;
+        mesh.Cell0DsCoordinates(0, i) = x;
+        mesh.Cell0DsCoordinates(1, i) = y;
+        mesh.Cell0DsCoordinates(2, i) = z;
+    }
+
+    return mesh;
+}
+
 
 }

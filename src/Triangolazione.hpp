7830682c9ff86ee3f546a0cf72f4a5b4977135f6
SofiaDiRosa <s297767@studenti.polito.it>
2025-06-13 01:37:30 +0200

    aggiornato Triangolazione,hpp

--- src/Triangolazione.hpp
+++ src/Triangolazione.hpp
@@ -3,10 +3,9 @@
 
 namespace PoliedriLibrary {
 
-	void TriangolaFacceClasseI(const PoliedriMesh &meshIniziale, PoliedriMesh &meshRisultato, unsigned int livelloSuddivisione);
-
 	unsigned int TrovaSpigolo(map<pair<unsigned int, unsigned int>, unsigned int> &mappaSpigoli, PoliedriMesh &mesh, unsigned int start, unsigned int end);
-
-	void TriangolaFacceClasseII(const PoliedriMesh &, PoliedriMesh &, unsigned int);
+	
+	void TriangolaFacceClasseI(const PoliedriMesh &meshIniziale, PoliedriMesh &meshRisultato, unsigned int livelloSuddivisione); //cos'è il livelloSuddivisione?
+	void TriangolaFacceClasseII(const PoliedriMesh &meshIniziale, PoliedriMesh &meshRisultato, unsigned int b);
 
 }

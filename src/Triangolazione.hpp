f5fab33c177483c71b7a26b7ec3e67e3e20ec994
SofiaDiRosa <s297767@studenti.polito.it>
2025-06-18 23:16:49 +0200

    modificato Triangolazione.hpp

--- src/Triangolazione.hpp
+++ src/Triangolazione.hpp
@@ -2,9 +2,19 @@
 #include "Utils.hpp"
 
 namespace PoliedriLibrary {
-
+	
+	// Ricerca degli spigoli per la costruzione della PoliedriMesh duale
+	// mesh: PoliedriMesh struct
+	// restituisce l'indice associato agli spigoli della mesh dopo aver eseguito la triangolazione per costruire la mesh duale
 	unsigned int TrovaSpigolo(map<pair<unsigned int, unsigned int>, unsigned int> &mappaSpigoli, PoliedriMesh &mesh, unsigned int start, unsigned int end);
 	
-	void TriangolaFacceClasseI(const PoliedriMesh &meshIniziale, PoliedriMesh &meshRisultato, unsigned int livelloSuddivisione); //cos'è il livelloSuddivisione?
-	void TriangolaFacceClasseII(const PoliedriMesh &meshIniziale, PoliedriMesh &meshRisultato, unsigned int b);
+	// Triangolazione delle facce dei solidi di classe 1
+	// mesh: PoliedriMesh struct
+	// restituisce una mesh triangola triangolando le facce del poliedro della meshIniziale secondo un livello di suddivisione regolare
+	void TriangolaFacceClasseI(const PoliedriMesh& meshIniziale, PoliedriMesh& meshRisultato, unsigned int livelloSuddivisione);
+	
+	// Triangolazione delle facce dei solidi di classe 2
+	// mesh: PoliedriMesh struct
+	// restituisce una mesh triangolata triangolando le facce del poliedro della meshIniziale secondo il livello di suddivisione b
+	void TriangolaFacceClasseII(const PoliedriMesh& meshIniziale, PoliedriMesh& meshRisultato, unsigned int b);
 }

7910a4176b5429dab4d6d0f2051eb02235b02599
SofiaDiRosa <s297767@studenti.polito.it>
2025-06-18 23:16:02 +0200

    modificato Duale.hpp

--- src/Duale.hpp
+++ src/Duale.hpp
@@ -2,10 +2,19 @@
 #include "Utils.hpp"
 
 namespace PoliedriLibrary {
-  
-	void CostruisciDualMesh(const PoliedriMesh& meshTriangolata, PoliedriMesh& meshDuale);
 
+	// Costruzione della meshTriangolata duale
+	// mesh: PoliedriMesh struct
+	// costruisce la mesh duale della mesh triangolata
+	void CostruisciDualMesh(const PoliedriMesh& meshTriangolata, PoliedriMesh& meshDuale);
+	
+	// 
+	// mesh: PoliedriMesh struct
+	// crea una mappa di coppie e vettori avendo in input la meshTriangolata
 	map<pair<unsigned int, unsigned int>, vector<unsigned int>> Mappa_LatoFaccia(const PoliedriMesh& meshTriangolata); //ex buildEdgesToFaces()
 	
+	// Proiezione sulla sfera unitaria
+	// mesh: PoliedriMesh struct
+	// fa in modo che la mesh ottenuta sia proiettata su una sfera unitaria
 	void ProiettaSuSfera(PoliedriMesh& mesh);
 }

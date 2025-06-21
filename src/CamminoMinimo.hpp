cb3c5f6cff150139e02a25ed905a53976bfd7a7b
SofiaDiRosa <s297767@studenti.polito.it>
2025-06-22 00:15:09 +0200

    modificato CamminoMinimo.hpp

--- src/CamminoMinimo.hpp
+++ src/CamminoMinimo.hpp
@@ -6,27 +6,28 @@ using namespace std;
 
 namespace PoliedriLibrary {
 
-class CamminoMinimo {
+class ShortestPath {
 public:
-    CamminoMinimo(const PoliedriMesh& mesh);
+    ShortestPath(const PoliedriMesh& mesh);
 
     // Calcola il cammino minimo tra due vertici
-    vector<unsigned int> CalcolaShortestPath(unsigned int startId, unsigned int endId, double& peso);
+    vector<unsigned int> CalcolaShortPath(unsigned int startId, unsigned int endId, double& totalLength);
 
     // Restituisce i vertici e lati marcati con ShortPath = 1
-    void CamminoMarcato(const vector<unsigned int>& cammino);
+    void MarcaCammino(const vector<unsigned int>& path);
 
     // Accesso ai vertici e lati marcati
-    const vector<int>& getVerticeMarcato() const;
-    const vector<int>& getLatoMarcato() const;
+    const vector<int>& getVerticiMarcati() const;
+    const vector<int>& getLatiMarcati() const;
 
 private:
     const PoliedriMesh& mesh;
-    vector<vector<pair<unsigned int, double>>> lista_adiacenza;
-    vector<int> vertice_marcato;
-    vector<int> lato_marcato;
+    vector<vector<pair<unsigned int, double>>> lista_adiacenza;  // lista di archi
+    vector<int> verticiMarcati;
+    vector<int> latiMarcati;
 
     void CostruisciGrafo();
+	
     double CalcolaDistanza(unsigned int id1, unsigned int id2);
 };
 

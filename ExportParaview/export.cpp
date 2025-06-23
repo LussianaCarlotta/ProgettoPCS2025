705fb2cba671a28f44953b3e98c87c2614612c67
SofiaDiRosa <s297767@studenti.polito.it>
2025-06-23 22:51:41 +0200

    modificato export.cpp

--- ExportParaview/export.cpp
+++ ExportParaview/export.cpp
@@ -32,7 +32,7 @@ void Export(const PoliedriMesh& mesh, const string& nomeBase) {
 
     // Export per i lati (Cell1Ds)
     vector<double> Cell1Ds_ShortPath(mesh.Cell1DsShortPath.begin(), mesh.Cell1DsShortPath.end());  // inizializzato copiando tutti i valori di mesh.Cell1DsShortPath e serve per definire quali lati fanno parte del cammino minimo e quali noboolalpha
-    vector<double> Cell1D_Ones(mesh.Cell1DsShortPath.size(), 1.0);  // ha la stessa dimensione di mesh.Cell1DsShortPath e inizializza tutti i valori a 1.0
+    //vector<double> Cell1D_Ones(mesh.Cell1DsShortPath.size(), 1.0);  // ha la stessa dimensione di mesh.Cell1DsShortPath e inizializza tutti i valori a 1.0
 
     vector<Gedim::UCDProperty<double>> lati_props;
     lati_props.push_back({
@@ -43,19 +43,19 @@ void Export(const PoliedriMesh& mesh, const string& nomeBase) {
 						  Cell1Ds_ShortPath.data()  // Data; puntatore diretto all'array di memoria utilizzato internamente dal vettore per memorizzare i propri elementi; può essere spostato per accedere a qualsiasi elemento dell'array
 						 });
 
-    lati_props.push_back({
+    /*lati_props.push_back({
 						  "CostanteUno",  // Label
 						  "",  // UnitLabel
 						  static_cast<unsigned int>(Cell1D_Ones.size()),  // Size
 						  1,  // NumComponents; proprietà scalare
 						  Cell1D_Ones.data()  // Data; puntatore diretto all'array di memoria utilizzato internamente dal vettore per memorizzare i propri elementi; può essere spostato per accedere a qualsiasi elemento dell'array
-						 });
+						 });*/
 
     utilities.ExportSegments(nomeBase + "_Cell1Ds.inp",
                              mesh.Cell0DsCoordinates,
                              mesh.Cell1DsExtrema,
                              {},
-                             lati_props); // Due proprietà: ShortPath e 1
+                             lati_props);
 }
 
 }
